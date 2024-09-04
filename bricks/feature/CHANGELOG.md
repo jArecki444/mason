afedb4a54e4df37ed337067c960506c453087d5f
jArecki444 <arkadiusz.cichurski@gmail.com>
2024-09-04 23:12:40 +0200

    Updated version of feature brick

--- bricks/feature/CHANGELOG.md
+++ bricks/feature/CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.1.0+2
+
+- Updated some imports, and UI widgets in body
+- 
 # 0.1.0+1
 
 - Initial release to generate data, domain & presentation layer
--- bricks/feature/__brick__/{{name}}_module.dart
+++ bricks/feature/__brick__/{{name}}_module.dart
@@ -3,7 +3,7 @@ import 'package:injectable/injectable.dart';
 
 import 'data/network/{{name}}_api.dart';
 import 'data/repository/rest_api_{{name}}_repository.dart';
-import 'domain/repository/{{name}}_repository.dart';
+import 'domain/repository/api_{{name}}_repository.dart';
 
 @module
 abstract class {{name.pascalCase()}}Module {
